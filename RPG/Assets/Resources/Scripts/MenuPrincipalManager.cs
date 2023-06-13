154e36eaebe86346f8b6d3789ea3743f92af2577
Vinicius Fontes <98784849+ViniciusFontes03@users.noreply.github.com>
2023-06-13 14:16:52 -0300

    Criação do menu básico

---
+++ RPG/Assets/Resources/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalManager : MonoBehaviour
+{
+    [SerializeField] private string nomeDoLevelDeJogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelOpcoes;
+
+    public void jogar()
+    {
+        SceneManager.LoadScene(nomeDoLevelDeJogo);
+    }
+    public void abrirOpcoes()
+    {
+        painelMenuInicial.SetActive(false);
+        painelOpcoes.SetActive(true);
+    }
+    public void fecharOpcoes()
+    {
+        painelOpcoes.SetActive(false);
+        painelMenuInicial.SetActive(true);
+    }
+    public void sairJogo()
+    {
+        Debug.Log("Saimento do jogo");
+        Application.Quit();
+    }
+}
