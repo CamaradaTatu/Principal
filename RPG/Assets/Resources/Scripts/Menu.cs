a0c44b917567f8c92e1cde3bc5063cf4680a274f
Vinicius Fontes <98784849+ViniciusFontes03@users.noreply.github.com>
2023-06-13 15:54:30 -0300

    Criação do menu de volumes

---
+++ RPG/Assets/Resources/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    float GetVol(float vol)
+    {
+        float newVol = 0;
+        newVol = 20 * Mathf.Log10(vol);
+        if (vol <= 0)
+        {
+            newVol = -80;
+        }
+        return newVol;
+    }
+
+    public void SetMasterVol(float vol)
+    {
+        mixer.SetFloat("MasterVol", GetVol(vol));
+    }
+
+    public void SetMusicVol(float vol)
+    {
+        mixer.SetFloat("MusicVol", GetVol(vol));
+    }
+
+    public void SetSFXVol(float vol)
+    {
+        mixer.SetFloat("SFXVol", GetVol(vol));
+    }
+}
