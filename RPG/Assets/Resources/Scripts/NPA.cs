4fb7d03216ce68fa33558ccd3b06d693959c049f
Ruanderson Carlos Menezes de Melo <123671300+CamaradaTatu@users.noreply.github.com>
2023-07-06 17:22:26 -0300

    coisas

---
+++ RPG/Assets/Resources/Scripts/NPA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPA : MonoBehaviour
+{
+    private Collider2D collider;
+    public Animator animator;
+
+    private void Start()
+    {
+        collider = GameObject.Find("MC").GetComponent<Collider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.Play("");
+    }
+
+}
