6d57fef4a7de45f9e9ff9d7263fd09aedef8ff41
Ruanderson Carlos Menezes de Melo <ruanderson123@gmail.com>
2023-07-09 19:28:47 -0300

    nda dms

---
+++ RPG/Assets/Resources/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    private Transform mcTransform;
+
+    private void Start()
+    {
+        mcTransform = FindObjectOfType<Char>().transform;
+    }
+
+    public void SpawnarObjeto()
+    {
+        Instantiate(objectPrefab, mcTransform.position, Quaternion.identity);
+    }
+}
